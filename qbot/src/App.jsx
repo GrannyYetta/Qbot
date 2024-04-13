87364c47b7d985ef1336abb8674912ccb9738102
Andreas Yin <andreas@Andreass-MacBook-Pro.local>
2024-04-13 17:32:56 +0200

    change description position

--- qbot/src/App.jsx
+++ qbot/src/App.jsx
@@ -1,34 +1,20 @@
-
-import "./styles.css"
+import './styles.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-    <div className="header">
-      <h1 className="jersey-10-regular">Meet Jamie, a non-judgemental counsellor developed by and for the queer
-        community</h1>
-      </div>
-      <div>
-        <ul className="krub-regular">
-=======
     <main className="container">
-      <h1>
+      <h1 className="jersey-10-regular">
         Meet Jamie, a non-judgemental counsellor developed by and for the queer
         community
       </h1>
-      <ul>
->>>>>>> 477b05e (position description)
+      <ul className="krub-regular">
         <li>at your fingertip</li>
         <li>listens and provides guidance</li>
         <li>safe and anonymous</li>
         <li>free to use</li>
       </ul>
-      </div>
-    </>
-  )
+    </main>
   );
-
 }
 
 export default App;
