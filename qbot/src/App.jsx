d14bd5a8564cbf7afb6e4c61641f42356da3c046
GrannyYetta <you@example.com>
2024-04-13 17:21:37 +0200

    first styling

--- qbot/src/App.jsx
+++ qbot/src/App.jsx
@@ -1,12 +1,25 @@
-import "./App.css"
+import "./styles.css"
 
 function App() {
  
 
   return (
     <>
-    <div className="header">Jamie QueerBot</div>
-      <h1>Hi, I'm Jamie. Welcome to the QueerBot! How can I help you?</h1>
+    <div className="header">
+      <h1 className="jersey-10-regular">Meet Jamie, a non-judgemental counsellor developed by and for the queer
+        community</h1>
+      </div>
+      <div>
+        <ul className="krub-regular">
+        <li>at your fingertip</li>
+        <li>listens and provides guidance</li>
+        <li>safe and anonymous</li>
+        <li>free to use</li>
+      </ul>
+      </div>
+  
+
+      
 
     </>
   )
